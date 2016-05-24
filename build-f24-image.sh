ede60c90a9a58d7e5e4d74a74ddef9a7a2638b5c
iamcourtney <cpacheco@redhat.com>
2016-05-24 10:02:53 -0400

    Removing unnecessary files

--- build-f24-image.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-sudo imagefactory --debug base_image --file-parameter install_script fedora-docker-base-custom.ks fedora-24-alpha.tdl --parameter offline_icicle true
-
--- build-rawhide-image.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-sudo imagefactory --debug base_image --file-parameter install_script fedora-docker-base-rawhide.ks fedora-rawhide.tdl --parameter offline_icicle true
